c4e231c381fe13b5ad7772a117dd317f8e9e46be
Da.L <luo_da@bentley.edu>
2015-04-19 21:11:43 -0400

    [DL] meeting layout updated

--- eventTrackerMap/src/com/example/eventtrackermap/newMeeting.java
+++ eventTrackerMap/src/com/example/eventtrackermap/newMeeting.java
@@ -48,11 +48,13 @@ public class newMeeting extends Activity{
 	private String location;
 	private String fromTime;
 	private String toTime;
+	private String description;
 	private EditText meetingSubject;
 	private EditText meetingLocation;
 	private TextView meetingDate;
 	private EditText meetingFromTime;
 	private EditText meetingToTime;
+	private EditText meetingDescription;
 	
 	
 	//initialize SQLite
@@ -82,6 +84,7 @@ public class newMeeting extends Activity{
         meetingLocation= (EditText)findViewById(R.id.meetingLocationEnter);//read entered location
         meetingFromTime= (EditText)findViewById(R.id.EnterTimefrom);//read entered from time
         meetingToTime= (EditText)findViewById(R.id.EnterTimeTo);//read entered to time
+        meetingDescription= (EditText)findViewById(R.id.meetingDescriptionEnter);//read entered description
         
         
         emailList = (ListView)findViewById(android.R.id.list);
@@ -226,14 +229,13 @@ public class newMeeting extends Activity{
 	     // Handle presses on the action bar items
 	     switch (item.getItemId()) { 
 	     	case R.id.sendEmail: //case 1, press send email button
-	     		storeMeeting();
 	     		sendEmail(); 
 	     		return true;
 	         case R.id.saving://case 2, press save button
-	        	 storeMeeting();
+	        	 //storeMeeting();
 	             return true;
 	         case R.id.viewMeeting://case 3, press viewmeeting button
-	        	 showMeeting(addContact);
+	        	 //showMeeting(addContact);
 	             return true;
 	         case R.id.exit://case 4, press exit button
 	        	 finish();
@@ -247,15 +249,22 @@ public class newMeeting extends Activity{
 	 protected void sendEmail() {
 	      Log.i("Send email", "");
 	      String[] TO = emails.toArray(new String[emails.size()]);
-	      subject= meetingSubject.getText().toString();
+	      subject= meetingSubject.getText().toString(); 
+		  date= meetingDate.getText().toString();
+		  location= meetingLocation.getText().toString();
+		  fromTime= meetingFromTime.getText().toString();
+		  toTime= meetingToTime.getText().toString();
+		  description= meetingDescription.getText().toString();
 	      Intent emailIntent = new Intent(Intent.ACTION_SEND);
 	      emailIntent.setData(Uri.parse("mailto:"));
 	      emailIntent.setType("text/plain");
 
-
+	      //insert recipients, subject and text content
 	      emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
 	      emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-	      emailIntent.putExtra(Intent.EXTRA_TEXT, "For testing purpose"); //need to write a method for output formatting
+	      emailIntent.putExtra(Intent.EXTRA_TEXT, "Details of the meeting invitation"+"\n"+"\n"+"Subject: "+subject+"\n"+
+	    		  				"Date: "+ date+ "\n"+ "Duration: "+fromTime+" - "+toTime+ "\n"+"Location: "+location+
+	    		  				"\n"+"Description: "+description+ "\n"+"\n"+"Please respond to this email if you can't attend. Thank you!"); 
 
 	      try {
 	         startActivity(Intent.createChooser(emailIntent, "Send mail..."));
@@ -268,68 +277,7 @@ public class newMeeting extends Activity{
 	   }
 	 
 	 
-	 //create method to store a meeting into datebase
-	 protected void storeMeeting(){
-		  subject= meetingSubject.getText().toString(); 
-		  date= meetingDate.getText().toString();
-		  location= meetingLocation.getText().toString();
-		  fromTime= meetingFromTime.getText().toString();
-		  toTime= meetingToTime.getText().toString();
-		  db.execSQL("INSERT INTO  meetings VALUES('"+subject+"','"+date+"','"+fromTime+"', '"+toTime+"', '"+location+"');");
-		  Toast.makeText(newMeeting.this, 
-			         "Your meeting has been saved.", Toast.LENGTH_SHORT).show();
-	 }
-	 
-	 //create method to display current meetings
-	 public void showMeeting(View view)
-	 {
-	    Cursor c=db.rawQuery("SELECT * from meetings", null);
-	    int count= c.getCount();
-	    c.moveToFirst();
-	    TableLayout tableLayout = new TableLayout(getApplicationContext());
-	    tableLayout.setVerticalScrollBarEnabled(true);
-	    TableRow tableRow;
-	    TextView textView,textView1,textView2,textView3,textView4,textView5;
-	    tableRow = new TableRow(getApplicationContext());
-	    textView=new TextView(getApplicationContext());
-	    textView.setText("Subject");
-	    textView.setTextColor(Color.RED);
-	   	textView.setTypeface(null, Typeface.BOLD);
-	   	textView.setPadding(20, 20, 20, 20);
-	   	tableRow.addView(textView);
-	    textView4=new TextView(getApplicationContext());
-	   	textView4.setText("Date");
-	   	textView4.setTextColor(Color.RED);
-	   	textView4.setTypeface(null, Typeface.BOLD);
-	   	 textView4.setPadding(20, 20, 20, 20);
-	   	tableRow.addView(textView4);
-	     textView5=new TextView(getApplicationContext());
-	   	textView5.setText("Time Begin");
-	   	textView5.setTextColor(Color.RED);
-	   	textView5.setTypeface(null, Typeface.BOLD);
-	   	textView5.setPadding(20, 20, 20, 20);
-	   	tableRow.addView(textView5);
-	    tableLayout.addView(tableRow);
-	      for (Integer j = 0; j < count; j++)
-	      {
-	          tableRow = new TableRow(getApplicationContext());
-	          textView1 = new TextView(getApplicationContext());
-	          textView1.setText(c.getString(c.getColumnIndex("subject")));
-	          textView2 = new TextView(getApplicationContext());
-	          textView2.setText(c.getString(c.getColumnIndex("date")));
-	          textView3 = new TextView(getApplicationContext());
-	          textView3.setText(c.getString(c.getColumnIndex("begintime")));
-	          textView1.setPadding(20, 20, 20, 20);
-	          textView2.setPadding(20, 20, 20, 20);
-	          textView3.setPadding(20, 20, 20, 20);
-	          tableRow.addView(textView1);
-	          tableRow.addView(textView2);
-	          tableRow.addView(textView3);
-	          tableLayout.addView(tableRow);
-	          c.moveToNext() ;
-	      }
-	      setContentView(tableLayout);
-	 db.close();
-	 }
+
+	  
 	 
 }
